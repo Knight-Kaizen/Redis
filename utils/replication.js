db57502a4c8a5813f08095f981c13c7585945e76
Knight-Kaizen <auferstehung20@gmail.com>
2024-09-24 00:01:22 +0530

    handshake 2: receive response for ping command, and send replconf commands

--- utils/replication.js
+++ utils/replication.js
@@ -3,18 +3,37 @@ const { parseResponse } = require('./commands');
 
 const client = new net.Socket();
 
-const sendHandshake = (flagsAndValues)=>{
-    if(flagsAndValues.replicaof){
+const sendHandshake = (flagsAndValues) => {
+    if (flagsAndValues.replicaof) {
         // running as a replica 
         const masterHost = flagsAndValues.replicaof.split(' ')[0];
         const masterPort = flagsAndValues.replicaof.split(' ')[1];
 
-        // Handshake 1: Send PING command to master
-        client.connect(masterPort, masterHost, function() {
-            
-            const command = parseResponse('bulkStringArray', ['PING'])
-            // console.log('sending ping command');
-            client.write(command);
+
+        client.connect(masterPort, masterHost, function () {
+            // console.log('connected to master');
+
+            // Handshake step 1: Send PING command to master
+            const ping = parseResponse('bulkStringArray', ['PING'])
+            client.write(ping);
+
+            // Wait for response, and once response received, send handshake step 2 commands
+        });
+
+        // Handle data received from the server
+        client.on('data', function (data) {
+            // console.log('Received: ' + data);
+
+            // Handshake step 2: Send REPLCONF command
+            // The REPLCONF command is used to configure replication. Replicas will send this command to the master twice
+
+            // notifying the master of the port it's listening on
+            const replconf1 = parseResponse('bulkStringArray', ['REPLCONF', 'listening-port', flagsAndValues.port])
+            client.write(replconf1);
+
+            // replica notifying the master of its capabilities ("capa" is short for "capabilities")
+            const replconf2 = parseResponse('bulkStringArray', ['REPLCONF', 'capa', 'psync2'])  // hardcoding capabilities for now
+            client.write(replconf2);
         });
 
     }
